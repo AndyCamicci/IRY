e64600a3ef930a08b2cec246d1c505eeb13891dc
Andy Camicci <andy.camicci@gmail.com>
2014-11-30 18:31:15 +0100

    src

---
+++ src/IRY/AppliBundle/Controller/ApplicationController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace IRY\AppliBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use IRY\AppliBundle\Entity\Helicopter;
+
+class ApplicationController extends Controller
+{
+    public function homeAction()
+    {
+        $em = $this->getDoctrine()->getManager(); // On récupère l'Entity Manager
+        $repo = $em->getRepository("IRYAppliBundle:Helicopter"); // On accède au Repository, qui possède les méthodes find(), findAll(), findBy() etc...
+        $listeHelicopteres = $repo->findAll();
+
+        return $this->render('IRYAppliBundle:Application:home.html.twig', array("data" => $listeHelicopteres));
+    }
+    public function choixcoursAction(Helicopter $helicopter_id)
+    {
+        return $this->render('IRYAppliBundle:Application:choixcours.html.twig', array("helicopter" => $helicopter_id));
+    }
+}
