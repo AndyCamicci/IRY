f27d44a262e3146fb1ee0770731904d58d13b0b2
yassine864 <yassine.moubarak@gmail.com>
2014-12-01 14:56:00 +0100

    creation de l entite Step

---
+++ src/IRY/AppliBundle/Entity/step.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace IRY\AppliBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+  /**
+     * @var \IRY\AppliBundle\Entity\step
+     */
+class step {
+	private $id;
+    private $course;
+    private $order;
+  
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Course
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set theme
+     *
+     * @param \IRY\AppliBundle\Entity\Theme $theme
+     * @return Course
+     */
+    public function setTheme(\IRY\AppliBundle\Entity\Theme $theme = null)
+    {
+        $this->theme = $theme;
+    
+        return $this;
+    }
+
+    /**
+     * Get theme
+     *
+     * @return \IRY\AppliBundle\Entity\Theme 
+     */
+    public function getTheme()
+    {
+        return $this->theme;
+    }
+}
