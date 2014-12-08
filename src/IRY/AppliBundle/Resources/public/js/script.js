559aff7747ec45f69e30c35ff4503df6997a3436
Andy Camicci <andy.camicci@gmail.com>
2014-12-08 19:45:19 +0100

    add JS for type filter and searchbar

---
+++ src/IRY/AppliBundle/Resources/public/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	/* FILTERS HELICOPTERS */
+	$(".helicopter_type").each(function() {
+		$(this).on("click", function() {
+			var type = $(this).attr("data-type");
+			showHelicoptersOfType(type);
+		});
+	});
+
+	/* SEARCH BAR HELICOPTERS */
+	$("input.search_content").on("keyup", function() {
+		var value = $(this).val();
+		filterHelicoptersByName(value);
+	});
+
+
+});
+
+function showHelicoptersOfType(type) {
+	$("a[data-type]").each(function() {
+		if ($(this).attr("data-type") == type) {
+			showHelicopter($(this));
+		} else {
+			hideHelicopter($(this));
+		}
+	});
+}
+
+function showHelicopter($helicopter) {
+	$helicopter.css("display", "block");
+}
+
+function hideHelicopter($helicopter) {
+	$helicopter.css("display", "none");
+}
+
+function filterHelicoptersByName(name) {
+	$("a[data-type]").each(function() {
+		if ($(this).attr("data-name").toLowerCase().indexOf(name.toLowerCase()) > -1) {
+			showHelicopter($(this));
+		} else {
+			hideHelicopter($(this));
+		}
+	});
+}
