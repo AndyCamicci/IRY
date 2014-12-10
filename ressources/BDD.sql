44e1175c14e5a3f8e018b65b2f312eda8fef544c
Andy Camicci <andy.camicci@gmail.com>
2014-12-10 14:37:50 +0100

    add ' in bdd

--- ressources/BDD.sql
+++ ressources/BDD.sql
@@ -61,7 +61,7 @@ INSERT INTO iry.course (id, subTheme_id, name, typeCourse_id) VALUES
 	(NULL, "7", "Filter clogging",                                "4"),
 	(NULL, "7", "Load factor",                                    "3");
 
-INSERT INTO `step` (id, course_id, name, order) VALUES
+INSERT INTO `step` (id, course_id, name, `order`) VALUES
 	(NULL, 1, 'demarage', 1),
 	(NULL, 1, 'stabilisation', 2),
 	(NULL, 1, 'tests', 3),
