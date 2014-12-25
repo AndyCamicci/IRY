b4a5ae01a1a34f2bf7381bc4de3acfaddd832e8f
Andy Camicci <andy.camicci@gmail.com>
2014-12-25 21:54:38 +0100

    Fix invalid entities configuration + add REST API base for pilots + composer update + new version of Unity Application

---
+++ src/IRY/AppliBundle/Form/Type/PilotType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace IRY\AppliBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PilotType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text');
+            // ->add('save', 'submit');
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+	    $resolver->setDefaults(array(
+	        'data_class' => 'IRY\AppliBundle\Entity\Pilot',
+            'csrf_protection' => false,
+	    ));
+	}
+
+    public function getName()
+    {
+        return 'pilot';
+    }
+}
